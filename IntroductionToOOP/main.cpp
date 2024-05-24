fc8b7442a64d3fcb214727e45166c288500efd1e
Igor <bignick13@yandex.ru>
2024-05-24 21:42:57 +0300

    Constructors

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -24,9 +24,58 @@ public:
 	{
 		this->y = y;
 	}
+	// Constructor
+	Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "1ArgConstructor:\t" << this << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
+	void print()const
+	{
+		cout << "X = " << x << "\tY = " << y << endl;
+	}
 };
+double distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	return sqrt(x_distance * x_distance + y_distance * y_distance);
+	
+}
+
 
 //#define STRUCT_POINT
+#define DISTANCE_CHEK
+//#define CONSTRUCTOR_CHEK
 
 void main()
 {
@@ -47,16 +96,32 @@ void main()
 	cout << sizeof(Point) << endl;
 #endif	// STRUCT_POINT
 
+#ifdef DISTANCE_CHEK
 	Point A;
 	A.set_x(2);
 	A.set_y(3);
 	cout << A.get_x() << "\t" << A.get_y() << endl;
-	/*
-	------------------------------------------------
 
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+	cout << "Расстояние от точки 'A' до точки 'B': " << A.distance(B) << endl;
+	cout << "Расстояние от точки 'B' до точки 'A': " << B.distance(A) << endl;
+	cout << "Расстояние между точками 'А' и 'В' : " << distance(A, B) << endl;
+	cout << "Расстояние между точками 'B' и 'A' : " << distance(A, B) << endl;
+#endif 
+#ifdef CONSTRUCTOR_CHEK	
+	Point A;
+	A.print();
 
+	Point B = 5;
+	B.print();
 
-	------------------------------------------------
-	*/
+	Point C(2, 3);
+	C.print();
 
+	Point D = C;
+	D.print();
+#endif
 }
