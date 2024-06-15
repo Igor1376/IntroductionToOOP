befc6126cb400d4b425a35378adf27e2a519fac6
Igor <bignick13@yandex.ru>
2024-06-15 22:01:37 +0300

    String DONE

--- String/main.cpp
+++ String/main.cpp
@@ -9,12 +9,40 @@ class String
 	int size; // размер строки в байтах
 	char* str; // адрес строки в динамической памяти
 public:
+	int get_size()const
+	{
+		return size;
+	}
 	// constructor
+	const char* get_str()const
+	{
+		return str;
+	}
+	char* get_str()
+	{
+		return str;
+	}
 	String(int size = 80)
 	{
 		this->size = size;
 		this->str = new char[size] {};
 		cout << "DefaultConstructor: \t" << this << endl;
+	}
+	String(const char str[])
+	{
+		this->size = strlen(str) + 1;
+		this->str = new char[size]{};
+		for (int i = 0; str[i]; i++)this->str[i] = str[i];
+		cout << "Constructor: \t\t" << this << endl;
+
+	}
+	String(const String& other)
+	{
+		//Deep copy
+		this->size = other.size;
+		this-> str = new char [size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyConstructor:\t" << this << endl;
 	}
 	~String()
 	{
@@ -22,12 +50,43 @@ public:
 		cout << "Destructor:\t\t" << this << endl;
 	}
 	// Methods
+	String& operator = (const String& other)
+	{
+		/*int a = 2;
+		int b = 3;
+		a = b;*/
+		if (this == &other)return *this;
+		delete[] str;
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
 	void print() const
 	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
+		
+		cout << "Obj:\t"  << this  <<  "\t";
+		cout << "Size:\t" << size <<  "\t";
+		cout << "Addr:\t" << &str <<  "\t";
+		cout << "Str:\t"  << str   <<  "\n";
 	}
 };
+std::ostream& operator << (std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+
+String operator+(const String& left, const String& right)
+{
+	String buffer = left.get_size() + right.get_size() - 1; // from int to string
+	for (int i = 0; i < left.get_size(); i++)	
+		buffer.get_str()[i] = left.get_str()[i];
+	for(int i = 0; i < right.get_size(); i++)
+		buffer.get_str()[i + left.get_size() -1] = right.get_str()[i];
+	buffer.print();
+	return buffer;
+}
 
 void main()
 {
@@ -36,10 +95,15 @@ void main()
 	//str.print();
 	String str1 = "Hello";
 	String str2 = "World";
+	str1 = str1;
 
 	cout << str1 << endl;
 	cout << str2 << endl;
 
-	String str3 = str1 + str2;
+	String str3 = str1 + str2; // copy constr
+	str3 = str1 + " " + str2;
+	//str3.print();
+	cout << str1 << endl;
+	cout << str2 << endl;
 	cout << str3 << endl;
 }
